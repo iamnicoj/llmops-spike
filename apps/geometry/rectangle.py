5d7b2b4ddd3d2fefd49a7173d962bdf9d61a5ba5
Crystal Atlaschi <crystalsm94@gmail.com>
2024-06-03 12:05:35 -0400

    testing rectangle changes

--- apps/geometry/rectangle.py
+++ apps/geometry/rectangle.py
@@ -1,7 +1,7 @@
 class Rectangle:
     def __init__(self, length, width):
         if length <= 0 or width <= 0:
-            raise ValueError("Length and width must be positive numbers.")
+            raise ValueError("Length and width must be positive numbers")
         self.length = length
         self.width = width
 
